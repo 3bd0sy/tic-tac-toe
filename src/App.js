6a952135b735708dfc3af268a18870396f848ee7
eng:3bdo <ax.abdo.syrain@gmail.com>
2023-08-11 00:40:52 +0300

    fix win function

--- src/App.js
+++ src/App.js
@@ -1,82 +1,106 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 function App() {
   let PlayerCode = ["X", "O"];
   let PlayerColor = ["#ff0000", "#00ff00"];
-  const [PlayerName, setPlayerName] = useState("PLAYER 1 IS PLAYING ...");
+  const [PlayerName, setPlayerName] = useState("X");
   const [playerID, setPlayerID] = useState(0);
   const [Divs, setDivs] = useState(Array(9).fill(""));
+  const [con, setCon] = useState(true);
+
+
   function init(index) {
     if (Divs[index] !== "X" && Divs[index] !== "O") {
-      ckeckWIN();
       let newDivs = [...Divs];
       newDivs[index] = PlayerCode[playerID];
       setDivs(newDivs);
       if (playerID === 0) {
         setPlayerID(1);
-        setPlayerName("PLAYER 2 IS PLAYING ...");
+        setPlayerName("O");
       } else {
         setPlayerID(0);
-        setPlayerName("PLAYER 1 IS PLAYING ...");
+        setPlayerName("X");
       }
-
     }
   }
 
+  useEffect(() => {
+    ckeckWIN();
+  }, [Divs]);
+
+
+
+  // function init(index) {
+  //   if (Divs[index] !== "X" && Divs[index] !== "O") {
+  //     let newDivs = [...Divs];
+  //     newDivs[index] = PlayerCode[playerID];
+  //     setDivs(newDivs);
+  //     if (playerID === 0) {
+  //       setPlayerID(1);
+  //       setPlayerName("O");
+  //     } else {
+  //       setPlayerID(0);
+  //       setPlayerName("X");
+  //     }
+  //     ckeckWIN()
+  //   }
+  // }
+
 
   function ckeckWIN() {
-    // Use the state variable Divs instead of the DOM elements
-    if (
-      (Divs[0] === PlayerCode[0] && Divs[1] === PlayerCode[0] && Divs[2] === PlayerCode[0]) ||
-      (Divs[3] === PlayerCode[0] && Divs[4] === PlayerCode[0] && Divs[5] === PlayerCode[0]) ||
-      (Divs[6] === PlayerCode[0] && Divs[7] === PlayerCode[0] && Divs[8] === PlayerCode[0]) ||
-      (Divs[0] === PlayerCode[0] && Divs[3] === PlayerCode[0] && Divs[6] === PlayerCode[0]) ||
-      (Divs[2] === PlayerCode[0] && Divs[5] === PlayerCode[0] && Divs[8] === PlayerCode[0]) ||
-      (Divs[1] === PlayerCode[0] && Divs[4] === PlayerCode[0] && Divs[7] === PlayerCode[0]) ||
-      (Divs[0] === PlayerCode[0] && Divs[4] === PlayerCode[0] && Divs[8] === PlayerCode[0]) ||
-      (Divs[2] === PlayerCode[0] && Divs[4] === PlayerCode[0] && Divs[6] === PlayerCode[0])
-    ) {
-      let newDivs = [...Divs];
-      console.log("player one [X] win................");
-      for (var i = 0; i < 9; i++) {
-        newDivs[i] = PlayerCode[0];
+    if (con) {
+      if (
+        (Divs[0] === PlayerCode[0] && Divs[1] === PlayerCode[0] && Divs[2] === PlayerCode[0]) ||
+        (Divs[3] === PlayerCode[0] && Divs[4] === PlayerCode[0] && Divs[5] === PlayerCode[0]) ||
+        (Divs[6] === PlayerCode[0] && Divs[7] === PlayerCode[0] && Divs[8] === PlayerCode[0]) ||
+        (Divs[0] === PlayerCode[0] && Divs[3] === PlayerCode[0] && Divs[6] === PlayerCode[0]) ||
+        (Divs[2] === PlayerCode[0] && Divs[5] === PlayerCode[0] && Divs[8] === PlayerCode[0]) ||
+        (Divs[1] === PlayerCode[0] && Divs[4] === PlayerCode[0] && Divs[7] === PlayerCode[0]) ||
+        (Divs[0] === PlayerCode[0] && Divs[4] === PlayerCode[0] && Divs[8] === PlayerCode[0]) ||
+        (Divs[2] === PlayerCode[0] && Divs[4] === PlayerCode[0] && Divs[6] === PlayerCode[0])
+      ) {
+        let newDivs = [...Divs];
+        for (var i = 0; i < newDivs.length; i++) {
+          newDivs[i] = PlayerCode[0];
+        }
+        setCon(false)
+        setDivs(newDivs);
+        alert("player X is the winner")
       }
-      setDivs(newDivs);
-      setPlayerName("PLAYER O IS THE WINER ...");
-    }
-    if (
-      (Divs[0] === PlayerCode[1] && Divs[1] === PlayerCode[1] && Divs[2] === PlayerCode[1]) ||
-      (Divs[3] === PlayerCode[1] && Divs[4] === PlayerCode[1] && Divs[5] === PlayerCode[1]) ||
-      (Divs[6] === PlayerCode[1] && Divs[7] === PlayerCode[1] && Divs[8] === PlayerCode[1]) ||
-      (Divs[0] === PlayerCode[1] && Divs[3] === PlayerCode[1] && Divs[6] === PlayerCode[1]) ||
-      (Divs[2] === PlayerCode[1] && Divs[5] === PlayerCode[1] && Divs[8] === PlayerCode[1]) ||
-      (Divs[1] === PlayerCode[1] && Divs[4] === PlayerCode[1] && Divs[7] === PlayerCode[1]) ||
-      (Divs[0] === PlayerCode[1] && Divs[4] === PlayerCode[1] && Divs[8] === PlayerCode[1]) ||
-      (Divs[2] === PlayerCode[1] && Divs[4] === PlayerCode[1] && Divs[6] === PlayerCode[1])
-    ) {
-      let newDivs = [...Divs];
-      console.log("player one [O] win................");
-      for (var i = 0; i < 9; i++) {
-        newDivs[i] = PlayerCode[1];
+      if (
+        (Divs[0] === PlayerCode[1] && Divs[1] === PlayerCode[1] && Divs[2] === PlayerCode[1]) ||
+        (Divs[3] === PlayerCode[1] && Divs[4] === PlayerCode[1] && Divs[5] === PlayerCode[1]) ||
+        (Divs[6] === PlayerCode[1] && Divs[7] === PlayerCode[1] && Divs[8] === PlayerCode[1]) ||
+        (Divs[0] === PlayerCode[1] && Divs[3] === PlayerCode[1] && Divs[6] === PlayerCode[1]) ||
+        (Divs[2] === PlayerCode[1] && Divs[5] === PlayerCode[1] && Divs[8] === PlayerCode[1]) ||
+        (Divs[1] === PlayerCode[1] && Divs[4] === PlayerCode[1] && Divs[7] === PlayerCode[1]) ||
+        (Divs[0] === PlayerCode[1] && Divs[4] === PlayerCode[1] && Divs[8] === PlayerCode[1]) ||
+        (Divs[2] === PlayerCode[1] && Divs[4] === PlayerCode[1] && Divs[6] === PlayerCode[1])
+      ) {
+        let newDivs = [...Divs];
+        for (var i = 0; i < newDivs.length; i++) {
+          newDivs[i] = PlayerCode[1];
+        }
+        setCon(false)
+        setDivs(newDivs);
+        alert("player O is the winner")
       }
-      // newDivs.map((d)=>{
-      // })
-      setDivs(newDivs);
-      setPlayerName("PLAYER X IS THE WINER ...");
     }
   }
 
 
   function reset() {
     setDivs(Array(9).fill(""));
-    setPlayerName("PLAYER 1 IS PLAYING ...");
+    setPlayerName("X");
+    setCon(true)
+
   }
 
   return (
     <>
       <h1>Tic Tac Toe</h1>
       <div id="menu">
-        <div id="playerName">{PlayerName}</div>
+        <div id="playerName">PLAYER <div className="span">{PlayerName}</div> IS PLAYING ...</div>
         <button className="reset" onClick={reset}>reset game</button>
       </div>
       <div className="page">
@@ -99,102 +123,3 @@ function App() {
 }
 
 export default App;
-
-
-
-// import './App.css';
-// import React, { useState } from 'react';
-// function App() {
-//   // const divs = [];
-//   // for (let i = 0; i < 9; i++) {
-//   //   divs.push(<div id={"d" + String(i)} onClick={init} style={{ backgroundColor: "#ffffff" }} className="grid-item"></div>);
-//   // }
-//   // console.log(divs[0])
-//   let PlayerCode = ["X", "O"];
-//   let PlayerColor = ["#ff0000", "#00ff00"];
-//   const [PlayerName, setPlayerName] = useState("PLAYER 1 IS PLAYING ...")
-//   const [playerID, setPlayerID] = useState(0)
-//   const [color, setColor] = useState("#ffffff");
-//   const [Divs, setDivs] = useState(Array(9).fill(""));
-
-//   function init(e) {
-//     if (e.target.innerHTML !== PlayerCode[0] && e.target.innerHTML !== PlayerCode[1]) {
-//       e.target.innerHTML = PlayerCode[playerID];
-//       e.target.style.background = PlayerColor[playerID];
-//       if (playerID === 0) {
-//         setPlayerID(1)
-//         setPlayerName("PLAYER 1 IS PLAYING ...")
-//       } else {
-//         setPlayerID(0)
-//         setPlayerName("PLAYER 1 IS PLAYING ...")
-//       }
-//       ckeckWIN();
-//     }
-//   }
-
-//   function ckeckWIN() {
-    // var buttons = document.getElementsByClassName("grid-item");
-    // if (buttons[0].innerHTML === PlayerCode[0] && buttons[1].innerHTML === PlayerCode[0] && buttons[2].innerHTML === PlayerCode[0] ||
-    //   buttons[3].innerHTML === PlayerCode[0] && buttons[4].innerHTML === PlayerCode[0] && buttons[5].innerHTML === PlayerCode[0] ||
-    //   buttons[6].innerHTML === PlayerCode[0] && buttons[7].innerHTML === PlayerCode[0] && buttons[8].innerHTML === PlayerCode[0] ||
-
-    //   buttons[0].innerHTML === PlayerCode[0] && buttons[3].innerHTML === PlayerCode[0] && buttons[6].innerHTML === PlayerCode[0] ||
-    //   buttons[2].innerHTML === PlayerCode[0] && buttons[5].innerHTML === PlayerCode[0] && buttons[8].innerHTML === PlayerCode[0] ||
-    //   buttons[1].innerHTML === PlayerCode[0] && buttons[4].innerHTML === PlayerCode[0] && buttons[7].innerHTML === PlayerCode[0] ||
-
-    //   buttons[0].innerHTML === PlayerCode[0] && buttons[4].innerHTML === PlayerCode[0] && buttons[8].innerHTML === PlayerCode[0] ||
-    //   buttons[2].innerHTML === PlayerCode[0] && buttons[4].innerHTML === PlayerCode[0] && buttons[6].innerHTML === PlayerCode[0]) {
-    //   console.log("player one [X] win................");
-    //   for (var i = 0; i < 9; i++) {
-    //     buttons[i].innerHTML = PlayerCode[0];
-    //     buttons[i].style.background = PlayerColor[0];
-    //     setPlayerName("PLAYER 1 IS THE WINER ...")
-    //   }
-//     }
-
-
-//     if (buttons[0].innerHTML === PlayerCode[1] && buttons[1].innerHTML === PlayerCode[1] && buttons[2].innerHTML === PlayerCode[1] ||
-//       buttons[3].innerHTML === PlayerCode[1] && buttons[4].innerHTML === PlayerCode[1] && buttons[5].innerHTML === PlayerCode[1] ||
-//       buttons[6].innerHTML === PlayerCode[1] && buttons[7].innerHTML === PlayerCode[1] && buttons[8].innerHTML === PlayerCode[1] ||
-
-//       buttons[0].innerHTML === PlayerCode[1] && buttons[3].innerHTML === PlayerCode[1] && buttons[6].innerHTML === PlayerCode[1] ||
-//       buttons[2].innerHTML === PlayerCode[1] && buttons[5].innerHTML === PlayerCode[1] && buttons[8].innerHTML === PlayerCode[1] ||
-//       buttons[1].innerHTML === PlayerCode[1] && buttons[4].innerHTML === PlayerCode[1] && buttons[7].innerHTML === PlayerCode[1] ||
-
-//       buttons[0].innerHTML === PlayerCode[1] && buttons[4].innerHTML === PlayerCode[1] && buttons[8].innerHTML === PlayerCode[1] ||
-//       buttons[2].innerHTML === PlayerCode[1] && buttons[4].innerHTML === PlayerCode[1] && buttons[6].innerHTML === PlayerCode[1]) {
-//       console.log("player two [O] win................");
-//       for (var j = 0; j < 9; j++) {
-//         buttons[j].innerHTML = PlayerCode[1];
-//         buttons[j].style.background = PlayerColor[1];
-//         setPlayerName("PLAYER 2 IS THE WINER ...")
-//       }
-//     }
-//   }
-//   function reset() {
-//     setDivs(Array(9).fill(""));
-//     setPlayerName("PLAYER 1 IS PLAYING ...");
-//     setColor("#5f5f5f")
-//   }
-
-//   return (
-//     <>
-//       <h1>Tic Tak Toy</h1>
-//       <div id="menu">
-//         <div id="playerName">{PlayerName}</div>
-//         <button onClick={reset}>reset game</button>
-//       </div>
-//       <div class="page">
-//         <div class="grid-container">
-//           {Divs.map((button, index) => (
-//             <div key={index} onClick={init} className="grid-item" style={{ background: color }}>
-//               {button}
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default App;
